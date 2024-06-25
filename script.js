a44b3e9a77d2a8a9009f2cf3a3d8a5fdb64b28e8
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-25 00:39:52 -1000

    Add pseudocode to second step
    Write logic to get the computer choice

---
+++ script.js
@@ -0,0 +1,9 @@
+// Step 1: Create the project setup
+
+// Step 2; Write the logic to get the computer choice
+/* PSEUDOCODE
+- create variable to hold computers choice in global scope
+- for numbers between 0 and 0.3333 return rock
+- for numbers 0.3334 and 0.6667 return paper
+- for numbers 0.6668 and 1 return scissors
+*/
