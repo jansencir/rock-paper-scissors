78742413c3a6f67ce0dd39f37ebfc9cd0d58bf11
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-06-25 11:34:15 -1000

    Create function to get computers choice
    Create function to return rock paper or scissors with the math random function

--- script.js
+++ script.js
@@ -7,3 +7,19 @@
 - for numbers 0.3334 and 0.6667 return paper
 - for numbers 0.6668 and 1 return scissors
 */
+
+let computerChoice;
+
+function getComputerChoice() {
+    computerNumber = Math.random()
+    if (computerNumber >= 0 && computerNumber <= 0.3333) {
+        computerChoice = "rock";
+        return computerChoice;
+    } else if (computerNumber >= 0.3334 && computerNumber <= 0.6667) {
+        computerChoice = "paper";
+        return computerChoice;
+    } else {
+        computerChoice = "scissors";
+        return computerChoice;
+    }
+}
