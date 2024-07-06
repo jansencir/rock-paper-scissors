5ea7584764fdae76b578c3cacda00aedb1fc8514
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-07-06 00:04:49 -1000

    Add variables to hold text and button elements

--- app.js
+++ app.js
@@ -14,35 +14,48 @@ function getComputerChoice() {
 
 
 // Logic to Get the Users Choice
-function getHumanChoice(humanChoice) {
-    if (humanChoice === "rock") {
-        return "rock";
-    } else if (humanChoice === "paper") {
-        return "paper";
-    } else if (humanChoice === "scissors") {
-        return "scissors";
-    } else {
-        alert("We didn't quite get that.")
-        getHumanChoice();
-    }
-}
+// function getHumanChoice(humanChoice) {
+//     if (humanChoice === "rock") {
+//         return "rock";
+//     } else if (humanChoice === "paper") {
+//         return "paper";
+//     } else if (humanChoice === "scissors") {
+//         return "scissors";
+//     } else {
+//         alert("We didn't quite get that.")
+//         getHumanChoice();
+//     }
+// }
 
 
 // Declare the players score variable
 let humanScore = 0;
 let computerScore = 0;
 
+// Scoreboard
+const playHeader = document.getElementById("playHeader");
+const playPara = document.getElementById("playPara");
+const playerScore = document.getElementById("humanScore");
+const compScore = document.getElementById("compScore");
+
+// Buttons
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
 
 // Play a single round of rock, paper, scissors
-function playRound(human, computer) {
-    if (human === computer) {
-        console.log("It was a tie, you and the computer chose the same thing!");
-        console.log(`You: ${humanScore} | Computer: ${computerScore}`);
+function playRound(human) {
+    const computerChoice = getComputerChoice();
+
+    if (human === computerChoice) {
+        playHeader.textContent = "It was a tie!";
+        playPara.textContent = `You and the computer both chose ${human}`;
     } else if (
         (human === "rock" && computer === "scissors") ||
         (human === "paper" && computer === "rock") ||
         (human === "scissors" && computer === "paper")
     ) {
+        playHeader.textContent = "You won!"
         console.log(`You won! ${human} beats ${computer}!`);
         ++humanScore;
         console.log(`You: ${humanScore} | Computer: ${computerScore}`);
